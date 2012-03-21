759e89dfa1972fdcf84cd0e5eb5dbbd125381cd3
mumurik <mumurik2@hotmail.co.jp>
2012-03-21 17:38:53 +0900

    Update patch level for update test.

--- src/version.h
+++ src/version.h
@@ -8,7 +8,7 @@
 # define PROGRAM_MINOR_VERSION 2
 # define PROGRAM_MAJOR_REVISION 3
 # define PROGRAM_MINOR_REVISION 5
-# define PROGRAM_PATCH_LEVEL 0
+# define PROGRAM_PATCH_LEVEL 1
 
 # define TITLE_BAR_STRING_SIZE 256
 extern char TitleBarString[];
