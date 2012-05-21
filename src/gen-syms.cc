39f03f26b1e0e3cef7d161956b1c5ef94c747f1e
mumurik <mumurik2@hotmail.co.jp>
2012-05-22 00:02:34 +0900

    FIX #37: Add (xyzzy-ini-path).
    See issue-tracker for detail. https://bitbucket.org/mumurik/xyzzy/issue/37/usb-xyzzyini

--- src/gen-syms.cc
+++ src/gen-syms.cc
@@ -1665,6 +1665,7 @@ static symbols ed[] =
   MAKE_SYMBOL2 (windows-7),
   MAKE_SYMBOL2 (windows-8),
   DEFUN3 (user-config-path, 0, 0, 0),
+  DEFUN3 (xyzzy-ini-path, 0, 0, 0),
   DEFVAR2 (*convert-registry-to-file-p*),
 
   /* Buffer.cc */
